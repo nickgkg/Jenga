d74c0e1604d350af63302cd82ed1fc4a2ef7fcc8
pbardea <pbardea@gmail.com>
2015-03-07 22:24:06 -0500

    Started scripting block generation

---
+++ Assets/BlockSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockSpawner : MonoBehaviour {
+
+	public GameObject block;
+	public GameObject[] blocks;
+
+	// Use this for initialization
+	void Start () {
+		blocks = new GameObject[54];
+		int height = 0;
+		int dim = 0;//number of blocks on a layer
+		for (;height < 18;height++){
+			for (;dim < 3; dim++){
+				GameObject cubeSpawn = (GameObject)Instantiate(block, new Vector3((float)0,(float)(dim*0.6),(float)height), transform.rotation);
+				blocks[dim+height*3] = cubeSpawn;
+			}
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
